d3c1907503b21c543c1e484584b8bfdb519fac0a
xavier-kong <kongxianchengxavier@gmail.com>
2024-06-27 21:05:49 +0800

    happy 3 year anniversary

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+
+  return (
+    <div></div>
+  );
+}
---
+++ src/app/streak/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Streaks() {
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
