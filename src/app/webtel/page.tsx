58c1b50747acb146259bf041094fa7515d19976b
xavier-kong <kongxianchengxavier@gmail.com>
2024-07-29 21:17:29 +0800

    passcord and id

--- src/app/webtel/page.tsx
+++ src/app/webtel/page.tsx
@@ -1,10 +1,15 @@
+import { api } from "~/trpc/react"
 
 
 export default function Home() {
+    const qrCodeUrl = api.post.generateQrCodeUrl.useQuery();
+
     return (
         <div className="bg-neutral-800 h-screen w-screen flex flex-col items-center justify-center">
             <div className="w-1/4 flex flex-col min-w-56">
-                <div className="h-52 w-full bg-blue-800"></div>
+                <div className="h-52 w-full bg-blue-800">
+
+                </div>
                 <div className="h-14 w-full bg-red-800">Scan the QR code above</div>
             </div>
         </div>
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,32 +1,28 @@
+import { randomUUID } from "crypto";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-let post = {
-  id: 1,
-  name: "Hello World",
-};
+const store: Record<string, string> = {};
 
 export const postRouter = createTRPCRouter({
+  generateQrCodeUrl: publicProcedure
+  .query(() => {
+    const uuid = randomUUID();
+
+    const passcode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    store[uuid] = passcode;
+
+    return {
+      uuid
+    };
+  }),
   hello: publicProcedure
   .input(z.object({ text: z.string() }))
   .query(({ input }) => {
     return {
       greeting: `Hello ${input.text}`,
     };
-  }),
-
-  create: publicProcedure
-  .input(z.object({ name: z.string().min(1) }))
-  .mutation(async ({ input }) => {
-    // simulate a slow db call
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    post = { id: post.id + 1, name: input.name };
-    return post;
-  }),
-
-  getLatest: publicProcedure.query(() => {
-    return post;
   }),
 });
