d7836273ce2c032698b7328b49092930a9675621
xavier-kong <kongxianchengxavier@gmail.com>
2024-07-31 20:24:15 +0800

    qr code styling

--- src/app/webtel/page.tsx
+++ src/app/webtel/page.tsx
@@ -1,8 +1,11 @@
-import { api } from "~/trpc/server";
-import { QRCode } from 'react-qrcode-logo';
+"use client"
 
-export default async function Home() {
-    const { uuid } = await api.webTel.generateQrCodeUrl();
+import { QRCode } from 'react-qrcode-logo';
+import { api } from '~/trpc/react';
+
+export default function Home() {
+    const res = api.webTel.generateQrCodeUrl.useQuery();
+
 
     // https://www.npmjs.com/package/react-qrcode-logo
 
@@ -10,7 +13,7 @@ export default async function Home() {
         <div className="bg-neutral-800 h-screen w-screen flex flex-col items-center justify-center">
             <div className="w-1/4 flex flex-col min-w-56">
                 <div className="h-52 w-full bg-blue-800 p-5 flex items-center justify-center">
-                    <QRCode value={`${uuid}`}/>
+                    <QRCode value={`${res.data?.uuid}`} eyeRadius={10} style={{ borderRadius: 20 }} />
                 </div>
                 <div className="h-14 w-full bg-red-800">Scan the QR code above</div>
             </div>
