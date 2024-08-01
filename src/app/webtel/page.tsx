08331abe999396ad5fc3d88299fd848a75c1ec84
xavier-kong <kongxianchengxavier@gmail.com>
2024-08-01 08:43:11 +0800

    shouldnt be so difficult to style this

--- src/app/webtel/page.tsx
+++ src/app/webtel/page.tsx
@@ -10,10 +10,20 @@ export default function Home() {
     // https://www.npmjs.com/package/react-qrcode-logo
 
     return (
-        <div className="bg-neutral-800 h-screen w-screen flex flex-col items-center justify-center">
-            <div className="w-1/4 flex flex-col min-w-56">
-                <div className="h-52 w-full bg-blue-800 p-5 flex items-center justify-center">
-                    <QRCode value={`${res.data?.uuid}`} eyeRadius={10} style={{ borderRadius: 20 }} />
+        <div className="bg-neutral-800 h-screen w-screen flex flex-col items-center justify-start">
+            <div className="w-1/4 flex flex-col min-w-56 mt-10">
+                <div className="h-72 w-full p-5 flex items-center justify-center">
+                    <QRCode 
+                        value={`${res.data?.uuid}`} 
+                        eyeRadius={10} 
+                        style={{ borderRadius: 20, height: '100%', width: '100%' }} 
+                        logoImage="https://w7.pngwing.com/pngs/673/14/png-transparent-circle-messenger-round-icon-telegram-popular-services-brands-vol-2-icon.png"
+                        removeQrCodeBehindLogo
+                        logoPadding={1}
+                        logoWidth={30}
+                        logoHeight={30}
+                        logoPaddingStyle="circle"
+                    />
                 </div>
                 <div className="h-14 w-full bg-red-800">Scan the QR code above</div>
             </div>
