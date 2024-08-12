874c488aa9541ef079995f3a6ffda53bb4d2aee2
xavier-kong <kongxianchengxavier@gmail.com>
2024-08-12 13:38:37 +0800

    header spacing

--- src/app/discord/page.tsx
+++ src/app/discord/page.tsx
@@ -3,15 +3,17 @@ import Image from "next/image";
 function Header() {
     return (
         <div className="flex flex-row h-12 justify-center mx-10">
-            <div className="w-36 items-center justify-center">
-                <Image 
-                    alt=""
-                    src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6257d23c5fb25be7e0b6e220_Open%20Source%20Projects%20_%20Discord-7.svg"
-                    width="900"
-                    height="900"
-                />
+            <div className="w-36 flex items-center justify-center">
+                <div className="w-24 flex items-center justify-center">
+                    <Image 
+                        alt=""
+                        src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6257d23c5fb25be7e0b6e220_Open%20Source%20Projects%20_%20Discord-7.svg"
+                        width="900"
+                        height="900"
+                    />
+                </div>
             </div>
-            <div className="flex flex-1 flex-row items-center text-white text-xs">
+            <div className="flex flex-1 flex-row items-center text-white text-xs px-16">
                 {
                     ['download', 'nitro', 'discord', 'safety', 'support', 'blog', 'careers'].map((item) => (
                         <div key={item} className="flex-1">
@@ -31,7 +33,7 @@ function Header() {
 
 function Hero() {
     return (
-        <div className="flex flex-row">
+        <div className="flex flex-row mt-20">
             <div className="flex flex-col flex-1">
                 <div>
                     <p>GROUP CHAT THAT’S ALL FUN & GAMES</p>
